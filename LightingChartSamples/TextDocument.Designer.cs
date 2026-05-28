845da4d11089cff0d7b773ea757e7cb15cf9025a
jspark <kordeveloper@outlook.com>
2026-05-29 03:16:30 +0900

    TextDocument 폼 추가 및 시작 폼 변경
    비즈니스 보고서 요약을 웹 스타일로 보여주는 TextDocument 폼을 새로 추가했습니다. 헤더, 본문, 하단 패널로 구성되어 있으며 RichTextBox를 활용해 다양한 서식의 텍스트를 출력합니다. UI에 라운드 및 그라데이션 효과를 적용했습니다. Program.cs의 시작 폼을 LightningBarSample에서 TextDocument로 변경하였고, 관련 파일을 csproj에 포함시켰습니다.

---
+++ LightingChartSamples/TextDocument.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace LightingChartSamples
+{
+    partial class TextDocument
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelRoot = new System.Windows.Forms.Panel();
+            this.panelCard = new System.Windows.Forms.Panel();
+            this.richTextBoxDocument = new System.Windows.Forms.RichTextBox();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.lblProgressValue = new System.Windows.Forms.Label();
+            this.progressImpact = new System.Windows.Forms.ProgressBar();
+            this.lblProgressTitle = new System.Windows.Forms.Label();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTag = new System.Windows.Forms.Label();
+            this.lblHeroSub = new System.Windows.Forms.Label();
+            this.lblHeroTitle = new System.Windows.Forms.Label();
+            this.panelRoot.SuspendLayout();
+            this.panelCard.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelRoot
+            // 
+            this.panelRoot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(251)))));
+            this.panelRoot.Controls.Add(this.panelCard);
+            this.panelRoot.Controls.Add(this.panelBottom);
+            this.panelRoot.Controls.Add(this.panelHeader);
+            this.panelRoot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRoot.Location = new System.Drawing.Point(0, 0);
+            this.panelRoot.Name = "panelRoot";
+            this.panelRoot.Padding = new System.Windows.Forms.Padding(24, 18, 24, 18);
+            this.panelRoot.Size = new System.Drawing.Size(900, 620);
+            this.panelRoot.TabIndex = 0;
+            // 
+            // panelCard
+            // 
+            this.panelCard.BackColor = System.Drawing.Color.White;
+            this.panelCard.Controls.Add(this.richTextBoxDocument);
+            this.panelCard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCard.Location = new System.Drawing.Point(24, 146);
+            this.panelCard.Name = "panelCard";
+            this.panelCard.Padding = new System.Windows.Forms.Padding(20, 18, 20, 18);
+            this.panelCard.Size = new System.Drawing.Size(852, 384);
+            this.panelCard.TabIndex = 2;
+            // 
+            // richTextBoxDocument
+            // 
+            this.richTextBoxDocument.BackColor = System.Drawing.Color.White;
+            this.richTextBoxDocument.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxDocument.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxDocument.Location = new System.Drawing.Point(20, 18);
+            this.richTextBoxDocument.Name = "richTextBoxDocument";
+            this.richTextBoxDocument.ReadOnly = true;
+            this.richTextBoxDocument.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBoxDocument.Size = new System.Drawing.Size(812, 348);
+            this.richTextBoxDocument.TabIndex = 0;
+            this.richTextBoxDocument.Text = "";
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.BackColor = System.Drawing.Color.White;
+            this.panelBottom.Controls.Add(this.lblProgressValue);
+            this.panelBottom.Controls.Add(this.progressImpact);
+            this.panelBottom.Controls.Add(this.lblProgressTitle);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(24, 530);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Padding = new System.Windows.Forms.Padding(18, 10, 18, 10);
+            this.panelBottom.Size = new System.Drawing.Size(852, 72);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // lblProgressValue
+            // 
+            this.lblProgressValue.AutoSize = true;
+            this.lblProgressValue.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.lblProgressValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(87)))), ((int)(((byte)(184)))));
+            this.lblProgressValue.Location = new System.Drawing.Point(768, 26);
+            this.lblProgressValue.Name = "lblProgressValue";
+            this.lblProgressValue.Size = new System.Drawing.Size(36, 19);
+            this.lblProgressValue.TabIndex = 2;
+            this.lblProgressValue.Text = "78%";
+            // 
+            // progressImpact
+            // 
+            this.progressImpact.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(123)))), ((int)(((byte)(213)))));
+            this.progressImpact.Location = new System.Drawing.Point(170, 22);
+            this.progressImpact.Name = "progressImpact";
+            this.progressImpact.Size = new System.Drawing.Size(580, 26);
+            this.progressImpact.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressImpact.TabIndex = 1;
+            this.progressImpact.Value = 78;
+            // 
+            // lblProgressTitle
+            // 
+            this.lblProgressTitle.AutoSize = true;
+            this.lblProgressTitle.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.lblProgressTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(80)))));
+            this.lblProgressTitle.Location = new System.Drawing.Point(21, 26);
+            this.lblProgressTitle.Name = "lblProgressTitle";
+            this.lblProgressTitle.Size = new System.Drawing.Size(125, 19);
+            this.lblProgressTitle.TabIndex = 0;
+            this.lblProgressTitle.Text = "마케팅 달성 전망";
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(129)))), ((int)(((byte)(226)))));
+            this.panelHeader.Controls.Add(this.lblTag);
+            this.panelHeader.Controls.Add(this.lblHeroSub);
+            this.panelHeader.Controls.Add(this.lblHeroTitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(24, 18);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Padding = new System.Windows.Forms.Padding(24, 16, 24, 16);
+            this.panelHeader.Size = new System.Drawing.Size(852, 128);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblTag
+            // 
+            this.lblTag.AutoSize = true;
+            this.lblTag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(236)))), ((int)(((byte)(255)))));
+            this.lblTag.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold);
+            this.lblTag.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(73)))), ((int)(((byte)(160)))));
+            this.lblTag.Location = new System.Drawing.Point(27, 87);
+            this.lblTag.Name = "lblTag";
+            this.lblTag.Padding = new System.Windows.Forms.Padding(10, 4, 10, 4);
+            this.lblTag.Size = new System.Drawing.Size(134, 23);
+            this.lblTag.TabIndex = 2;
+            this.lblTag.Text = "Q3 Executive Brief";
+            // 
+            // lblHeroSub
+            // 
+            this.lblHeroSub.AutoSize = true;
+            this.lblHeroSub.Font = new System.Drawing.Font("맑은 고딕", 10.5F);
+            this.lblHeroSub.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.lblHeroSub.Location = new System.Drawing.Point(27, 51);
+            this.lblHeroSub.Name = "lblHeroSub";
+            this.lblHeroSub.Size = new System.Drawing.Size(376, 19);
+            this.lblHeroSub.TabIndex = 1;
+            this.lblHeroSub.Text = "실행 중심 요약 · KPI 기반 인사이트 · 고객 반응 개선 시나리오";
+            // 
+            // lblHeroTitle
+            // 
+            this.lblHeroTitle.AutoSize = true;
+            this.lblHeroTitle.Font = new System.Drawing.Font("맑은 고딕", 17F, System.Drawing.FontStyle.Bold);
+            this.lblHeroTitle.ForeColor = System.Drawing.Color.White;
+            this.lblHeroTitle.Location = new System.Drawing.Point(25, 16);
+            this.lblHeroTitle.Name = "lblHeroTitle";
+            this.lblHeroTitle.Size = new System.Drawing.Size(415, 31);
+            this.lblHeroTitle.TabIndex = 0;
+            this.lblHeroTitle.Text = "📊 비즈니스 보고서 요약 대시보드 템플릿";
+            // 
+            // TextDocument
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 620);
+            this.Controls.Add(this.panelRoot);
+            this.MinimumSize = new System.Drawing.Size(900, 620);
+            this.Name = "TextDocument";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Styled Report";
+            this.panelRoot.ResumeLayout(false);
+            this.panelCard.ResumeLayout(false);
+            this.panelBottom.ResumeLayout(false);
+            this.panelBottom.PerformLayout();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelRoot;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblHeroTitle;
+        private System.Windows.Forms.Label lblHeroSub;
+        private System.Windows.Forms.Label lblTag;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Label lblProgressTitle;
+        private System.Windows.Forms.ProgressBar progressImpact;
+        private System.Windows.Forms.Label lblProgressValue;
+        private System.Windows.Forms.Panel panelCard;
+        private System.Windows.Forms.RichTextBox richTextBoxDocument;
+    }
+}
---
+++ LightingChartSamples/TextDocument.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace LightingChartSamples
+{
+    public partial class TextDocument : Form
+    {
+        public TextDocument()
+        {
+            InitializeComponent();
+            Load += TextDocument_Load;
+            SizeChanged += TextDocument_SizeChanged;
+            BuildDocument();
+        }
+
+        private void TextDocument_Load(object sender, EventArgs e)
+        {
+            ApplyWebStyle();
+        }
+
+        private void TextDocument_SizeChanged(object sender, EventArgs e)
+        {
+            ApplyWebStyle();
+        }
+
+        private void ApplyWebStyle()
+        {
+            panelHeader.Paint -= PanelHeader_Paint;
+            panelHeader.Paint += PanelHeader_Paint;
+
+            ApplyRoundedRegion(panelHeader, 18);
+            ApplyRoundedRegion(panelCard, 14);
+            ApplyRoundedRegion(panelBottom, 12);
+        }
+
+        private void PanelHeader_Paint(object sender, PaintEventArgs e)
+        {
+            using (var brush = new LinearGradientBrush(panelHeader.ClientRectangle,
+                Color.FromArgb(70, 118, 210),
+                Color.FromArgb(103, 158, 245),
+                20f))
+            {
+                e.Graphics.FillRectangle(brush, panelHeader.ClientRectangle);
+            }
+        }
+
+        private void ApplyRoundedRegion(Control control, int radius)
+        {
+            if (control.Width <= 0 || control.Height <= 0)
+            {
+                return;
+            }
+
+            var rect = new Rectangle(0, 0, control.Width, control.Height);
+            var diameter = radius * 2;
+            var path = new GraphicsPath();
+
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+
+            control.Region = new Region(path);
+        }
+
+        private void BuildDocument()
+        {
+            richTextBoxDocument.Clear();
+            richTextBoxDocument.DetectUrls = true;
+            richTextBoxDocument.SelectionIndent = 0;
+            richTextBoxDocument.SelectionHangingIndent = 0;
+            var ivoryBackColor = Color.FromArgb(255, 255, 240);
+
+            var titleFont = new Font("맑은 고딕", 14F, FontStyle.Bold);
+            var headingFont = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            var bodyFont = new Font("맑은 고딕", 11F, FontStyle.Regular);
+            var tipFont = new Font("맑은 고딕", 10.5F, FontStyle.Regular);
+            var chipFont = new Font("맑은 고딕", 9.5F, FontStyle.Bold);
+
+            AppendStyledText("3. 비즈니스 보고서 요약 샘플 (개조식)\n", titleFont, Color.Black);
+            AppendStyledText("  LIVE  ", chipFont, Color.White, Color.FromArgb(236, 95, 80));
+            AppendStyledText("  KPI TRACKING  ", chipFont, Color.White, Color.FromArgb(58, 136, 255));
+            AppendStyledText("\n\n", bodyFont, Color.Black);
+            AppendStyledText("실제 업무에서 사용하는 결론 중심의 핵심 보고서 텍스트 양식입니다.\n", bodyFont, Color.FromArgb(57, 67, 89));
+            AppendStyledText("────────────────────────────────────────────────────\n\n", bodyFont, Color.FromArgb(225, 229, 236));
+
+            AppendBulletHeading("[추진 배경] ", headingFont, bodyFont);
+            AppendStyledText("분기별 매출 감소에 따른 온·오프라인 채널 통합 마케팅 전략 변경 필요성 대두\n\n", bodyFont, Color.Black, ivoryBackColor);
+
+            AppendBulletHeading("[개선 방안]\n", headingFont, bodyFont);
+            AppendSubBullet("모바일 앱 UI/UX 개편을 통한 사용자 편의성 증대", bodyFont, ivoryBackColor);
+            AppendSubBullet("타겟 고객층(2030 세대) 맞춤형 프로모션 전개\n", bodyFont, ivoryBackColor);
+
+            AppendBulletHeading("[기대 효과] ", headingFont, bodyFont);
+            AppendStyledText("신규 고객 유입률 ", bodyFont, Color.Black, ivoryBackColor);
+            AppendStyledText("15%", new Font("맑은 고딕", 11F, FontStyle.Bold), Color.FromArgb(35, 84, 148), ivoryBackColor);
+            AppendStyledText(" 증가 및 기존 고객 이탈율 감소 기대\n\n", bodyFont, Color.Black, ivoryBackColor);
+
+            AppendStyledText("📌 Next Action: ", new Font("맑은 고딕", 10.5F, FontStyle.Bold), Color.FromArgb(60, 85, 128));
+            AppendStyledText("다음 주까지 A/B 테스트 2종과 채널별 CAC 리포트 업데이트\n", bodyFont, Color.FromArgb(72, 84, 104));
+
+            AppendTipBox(tipFont);
+
+            richTextBoxDocument.SelectionStart = 0;
+            richTextBoxDocument.SelectionLength = 0;
+            richTextBoxDocument.ScrollToCaret();
+        }
+
+        private void AppendBulletHeading(string text, Font headingFont, Font bodyFont)
+        {
+            richTextBoxDocument.SelectionBullet = true;
+            richTextBoxDocument.SelectionIndent = 0;
+            richTextBoxDocument.SelectionHangingIndent = 0;
+            AppendStyledText(text, headingFont, Color.Red);
+            richTextBoxDocument.SelectionBullet = false;
+            richTextBoxDocument.SelectionIndent = 0;
+            richTextBoxDocument.SelectionHangingIndent = 0;
+            richTextBoxDocument.SelectionFont = bodyFont;
+        }
+
+        private void AppendSubBullet(string text, Font font, Color backColor)
+        {
+            richTextBoxDocument.SelectionIndent = 25;
+            richTextBoxDocument.SelectionHangingIndent = 15;
+            richTextBoxDocument.SelectionBullet = true;
+            AppendStyledText(text + "\n", font, Color.Black, backColor);
+            richTextBoxDocument.SelectionBullet = false;
+            richTextBoxDocument.SelectionIndent = 0;
+            richTextBoxDocument.SelectionHangingIndent = 0;
+        }
+
+        private void AppendTipBox(Font tipFont)
+        {
+            AppendStyledText("\n", tipFont, Color.Black);
+            var tipBackColor = Color.FromArgb(237, 239, 242);
+            var tipForeColor = Color.FromArgb(35, 44, 62);
+
+            AppendStyledText("   💡 Tip: ", new Font("맑은 고딕", 10.5F, FontStyle.Bold), tipForeColor, tipBackColor);
+            AppendStyledText("한글 문장의 다양한 더미 텍스트가 필요하다면 ", tipFont, tipForeColor, tipBackColor);
+            AppendStyledText("한글 Lorem Ipsum 생성기", new Font("맑은 고딕", 10.5F, FontStyle.Underline), Color.FromArgb(26, 64, 140), tipBackColor);
+            AppendStyledText("를 통해 맞춤형 테스트 문자열을 생성하실 수 있습니다.", tipFont, tipForeColor, tipBackColor);
+            AppendStyledText("   \n", tipFont, tipForeColor, tipBackColor);
+        }
+
+        private void AppendStyledText(string text, Font font, Color foreColor, Color? backColor = null)
+        {
+            richTextBoxDocument.SelectionStart = richTextBoxDocument.TextLength;
+            richTextBoxDocument.SelectionLength = 0;
+            richTextBoxDocument.SelectionFont = font;
+            richTextBoxDocument.SelectionColor = foreColor;
+            richTextBoxDocument.SelectionBackColor = backColor ?? richTextBoxDocument.BackColor;
+            richTextBoxDocument.AppendText(text);
+            richTextBoxDocument.SelectionBackColor = richTextBoxDocument.BackColor;
+        }
+    }
+}
